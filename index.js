3e106768d7fc205d323768b8138062664f7a82f0
YunosukeYoshino <26004836+YunosukeYoshino@users.noreply.github.com>
2022-04-23 00:54:35 +0900

    ✨ 空文字の時に入力が出来なくなる機能を追加しました。

--- index.js
+++ index.js
@@ -1,46 +1,47 @@
-  new Vue({
-    el: "#app",
-    data: {
-      title: "My Todo App",
-      tasks: [],
-      newTask: ""
-    },
-    mounted() {
-      this.getLocalStorageData()
-    },
-    methods: {
-      addTask() {
-        const task = {
-          name: this.newTask,
-          isComplete: false
-        }
+new Vue({
+  el: "#app",
+  data: {
+    title: "My Todo App",
+    tasks: [],
+    newTask: ""
+  },
+  mounted() {
+    this.getLocalStorageData()
+  },
+  methods: {
+    addTask() {
+      const task = {
+        name: this.newTask,
+        isComplete: false
+      }
 
+      task.name === "" ? alert("タスクを追加してね💏") :
         this.tasks.push(task)
-        localStorage.setItem('todo', JSON.stringify(this.tasks));
-        this.newTask = ""
-      },
-      deleteTask(index) {
-        this.tasks.forEach((task, loopIndex) => {
-          if (index === loopIndex) {
-            this.tasks.splice(index, 1);
-          }
-        })
-        localStorage.setItem('todo', JSON.stringify(this.tasks));
-      },
-      completeTask(index) {
-        this.tasks.forEach((task, loopIndex) => {
-          if (index === loopIndex) {
-            task.isComplete = !task.isComplete
-          }
-        })
-        localStorage.setItem('todo', JSON.stringify(this.tasks));
-      },
-      getLocalStorageData() {
-        const todo = localStorage.getItem('todo')
-
-        if (todo) {
-          this.tasks = JSON.parse(todo)
+      localStorage.setItem('todo', JSON.stringify(this.tasks));
+      this.newTask = "";
+    },
+    deleteTask(index) {
+      this.tasks.forEach((task, loopIndex) => {
+        if (index === loopIndex) {
+          this.tasks.splice(index, 1);
         }
+      })
+      localStorage.setItem('todo', JSON.stringify(this.tasks));
+    },
+    completeTask(index) {
+      this.tasks.forEach((task, loopIndex) => {
+        if (index === loopIndex) {
+          task.isComplete = !task.isComplete
+        }
+      })
+      localStorage.setItem('todo', JSON.stringify(this.tasks));
+    },
+    getLocalStorageData() {
+      const todo = localStorage.getItem('todo')
+
+      if (todo) {
+        this.tasks = JSON.parse(todo)
       }
     }
-  })
+  }
+})
